c88ca3eb518af780c778f60fe423fbb7db219a11
adu <nogo22@naver.com>
2022-01-11 03:04:03 +0900

    Nickname, duplicate check.

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,20 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException } from '@nestjs/common';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      }
+      console.error(error);
+    }
+  }
+}
